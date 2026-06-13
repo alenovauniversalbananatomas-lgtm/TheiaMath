963a44ba3333edba5542c015462056b0b8d5df9b
alenovauniversalbananatomas-lgtm <alenovauniversalbananatomas@gmail.com>
2026-06-13 18:55:09 -0300

    Actualizar Arithmetic.java

--- src/theia/math/Arithmetic.java
+++ src/theia/math/Arithmetic.java
@@ -2,50 +2,47 @@ package theia.math;
 
 public class Arithmetic {
 
-    // SUMA
     public static double Add(double a, double b) {
         return a + b;
     }
 
-    // RESTA
     public static double Sub(double a, double b) {
         return a - b;
     }
 
-    // MULTIPLICACIÓN
     public static double Mul(double a, double b) {
         return a * b;
     }
 
-    // DIVISIÓN
     public static double Div(double a, double b) {
-        if (b == 0) {
+        if (b != 0) {
+            return a / b;
+        } else {
             throw new ArithmeticException("Division by zero");
         }
-        return a / b;
     }
 
-    // DIVISIÓN ENTERA
     public static int Idiv(double a, double b) {
-        if (b == 0) {
+        if (b != 0) {
+            return (int) (a / b);
+        } else {
             throw new ArithmeticException("Division by zero");
         }
-        return (int) (a / b);
     }
 
-    // RESTO
     public static double Mod(double a, double b) {
-        if (b == 0) {
+        if (b != 0) {
+            return a % b;
+        } else {
             throw new ArithmeticException("Division by zero");
         }
-        return a % b;
     }
 
-    // RESTO ENTERO
     public static int Imod(double a, double b) {
-        if (b == 0) {
+        if (b != 0) {
+            return (int) (a % b);
+        } else {
             throw new ArithmeticException("Division by zero");
         }
-        return (int) (a % b);
     }
 }
